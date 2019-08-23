277ae9eb6688fdef30440d55989bfa63c66e70dd
dtmkeng <kang081@hotmail.com>
2019-08-23 23:24:35 +0700

    update

--- src/main/java/com/example/demo/PostController.java
+++ src/main/java/com/example/demo/PostController.java
@@ -30,11 +30,11 @@ public class PostController {
     public Post Post() {
         return postRepository.findByTitle("test");
     }
-    @GetMapping("/comment/{id}")
-    public Collection<Comment> Customers(@PathVariable Long id) {
-        Post ps = postRepository.findByPostId(id);
-        Collection<Comment> commnet = commentRepository.findByPost(ps);
-        return commnet;
-    }
+    // @GetMapping("/comment/{id}")
+    // public Collection<Comment> Customers(@PathVariable Long id) {
+    //     Post ps = postRepository.findByPostId(id);
+    //     Collection<Comment> commnet = commentRepository.findByPost(ps);
+    //     return commnet;
+    // }
 
 }
